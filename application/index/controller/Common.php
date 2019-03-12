0f49061f26867eeefbfac9fde1d7c0f582bc196a
lidan <737842620@qq.com>
2019-03-12 13:35:23 +0800

    001提交，新增index和admin模块

---
+++ application/index/controller/Common.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AndreaDittis
+ * Date: 2019/3/6 0006
+ * Time: 上午 9:29
+ */
+
+namespace app\index\controller;
+use think\Controller;
+
+class Common extends Controller{
+
+
+
+
+
+
+} 
